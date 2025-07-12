08a2e21ccd5c9cf484d81c3d79aeba2075e9163b
Aniket Mukherjee <an469681@dal.ca>
2025-07-12 00:27:33 -0300

    ready netlify environment variable

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -17,7 +17,8 @@ import {
 } from '../constants/productConstants'
 
 // API Base URL
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`
+
 
 // Axios instance with default config
 const api = axios.create({
